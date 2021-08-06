2f4105b9393737b07a22a0ab8f6b2311284b5773
Egor <davidovich94@mail.ru>
2021-08-06 14:45:46 +0300

    todo v1

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { setupApi } from './api';
 import AppContext from './AppContext';
-import LoginPage from './LoginPage';
-import TodoPage from './TodoPage';
+import LoginPage from './pages/LoginPage';
+import TodoPage from './pages/TodoPage';
 
 export default function App() {
+  const [isInitialized, setIsInitialized] = useState(false);
+
+  useEffect(() => {
+    const { token } = window.sessionStorage;
+    if (token) setupApi(token);
+    setIsInitialized(true);
+  }, []);
+
+  if (!isInitialized) return null;
+
   return (
     <AppContext>
       <Router>
         <Switch>
-          <Route path="/login">
-            <LoginPage />
-          </Route>
           <Route path="/" exact>
             <TodoPage />
+          </Route>
+          <Route path="/login">
+            <LoginPage />
           </Route>
           <Route>
             404 not found <Link to="/login">login</Link>
